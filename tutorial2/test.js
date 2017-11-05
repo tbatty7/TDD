730ccf4ea8f869c78a7d7461f6124bae13e57cb8
Timothy Batty <tbatty7@gmail.com>
2017-11-04 22:45:41 -0400

    Tutorial 2, Roman Numeral Converter tests started with valid initial module test

---
+++ tutorial2/test.js
@@ -0,0 +1,10 @@
+const expect = require('chai').expect;
+
+describe('Tests for Roman Numeral Converter', ()=>{
+	it('There should be a valid module', ()=>{
+		const romanNumerals = require('./romanNumerals');
+		
+		
+		
+	});
+});
